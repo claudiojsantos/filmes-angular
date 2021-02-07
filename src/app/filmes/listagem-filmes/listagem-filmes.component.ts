28abf5f54171aec6c8f37d334d869f1675470dc3
Cláudio Santos <claudio@sistnet.com.br>
2021-02-07 15:23:10 -0300

    melhorando performance de consulta por filtro

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 import { FilmesService } from 'src/app/core/filmes.service';
 import { ConfigParams } from 'src/app/shared/models/config-params';
 import { Filme } from 'src/app/shared/models/filme';
@@ -10,6 +11,7 @@ import { Filme } from 'src/app/shared/models/filme';
   styleUrls: ['./listagem-filmes.component.scss']
 })
 export class ListagemFilmesComponent implements OnInit {
+  readonly semFoto = "https://triunfo.pe.gov.br/pm_tr430/wp-content/uploads/2018/03/sem-foto.jpg";
 
   config: ConfigParams = {
     pagina: 0,
@@ -30,9 +32,11 @@ export class ListagemFilmesComponent implements OnInit {
       }
     )
 
-    this.filtrosListagem.get('texto').valueChanges.subscribe((val: string) => {
-      this.config.pesquisa = val
-      this.resetarConsulta()
+    this.filtrosListagem.get('texto').valueChanges
+      .pipe(debounceTime(400))
+      .subscribe((val: string) => {
+        this.config.pesquisa = val
+        this.resetarConsulta()
     })
 
     this.filtrosListagem.get('genero').valueChanges.subscribe((val: string) => {
