014759534567e62dfbb3ec91435fb1d5dcb75605
Cláudio Santos <claudio@sistnet.com.br>
2021-02-07 21:58:48 -0300

    excluindo os filmes

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { debounceTime } from 'rxjs/operators';
 import { FilmesService } from 'src/app/core/filmes.service';
 import { ConfigParams } from 'src/app/shared/models/config-params';
@@ -22,7 +23,8 @@ export class ListagemFilmesComponent implements OnInit {
   generos: Array<string>
 
   constructor(private filmesService: FilmesService,
-    private fb: FormBuilder) { }
+    private fb: FormBuilder,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.filtrosListagem = this.fb.group(
@@ -51,6 +53,10 @@ export class ListagemFilmesComponent implements OnInit {
 
   onScroll(): void {
     this.listaFilmes()
+  }
+
+  abrir(id: number): void {
+    this.router.navigateByUrl('/filmes/' + id)
   }
 
   private listaFilmes(): void {
